36bdef3bd1b9e43a7959bf3ea5d9aba2f49d48e1
Ming Di Leom <2809763-curben@users.noreply.gitlab.com>
2021-05-22 03:06:13 +0000

    fix: json files are now categorised by country

--- src/script.js
+++ src/script.js
@@ -13,14 +13,17 @@ const f = async () => {
 # License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 # Source: https://github.com/duckduckgo/tracker-radar\n`)
   const domains = join(__dirname, '../tmp/tracker-radar/domains')
-  const files = await readdir(domains)
-  for (const file of files) {
-    const data = await readFile(join(domains, file))
-    const { resources } = parse(data)
-    const tracking = resources.filter(({ fingerprinting }) => fingerprinting === 1)
-    for (const { rule } of tracking) {
-      const link = rule.replace(/\\/g, '') + '\n'
-      await appendFile(outputFile, link)
+  const countries = await readdir(domains)
+  for (const country of countries) {
+    const files = await readdir(join(domains, country))
+    for (const file of files) {
+      const data = await readFile(join(domains, country, file))
+      const { resources } = parse(data)
+      const tracking = resources.filter(({ fingerprinting }) => fingerprinting === 1)
+      for (const { rule } of tracking) {
+        const link = rule.replace(/\\/g, '') + '\n'
+        await appendFile(outputFile, link)
+      }
     }
   }
 }
